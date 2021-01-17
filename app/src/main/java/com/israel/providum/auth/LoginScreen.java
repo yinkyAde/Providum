15edc432d52fb65d15bef0dc5ccf820fc3d57400
Israel Aderoju <yinkyade@gmail.com>
2021-01-17 18:25:55 +0100

    Login Screen

---
+++ app/src/main/java/com/israel/providum/auth/LoginScreen.java
@@ -0,0 +1,16 @@
+package com.israel.providum.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.israel.providum.R;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
