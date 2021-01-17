cf4268cbace246cb04679e0f7ca36a61c5c26dbb
Israel Aderoju <yinkyade@gmail.com>
2021-01-17 18:47:02 +0100

    Login Screen

--- app/src/main/java/com/israel/providum/auth/LoginScreen.java
+++ app/src/main/java/com/israel/providum/auth/LoginScreen.java
@@ -1,8 +1,8 @@
 package com.israel.providum.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.israel.providum.R;
 
